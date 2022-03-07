e880198bf545c7395c4508eba2c3c731c04c533f
Horacio Valdes <horavaldesg@gmail.com>
2022-03-06 19:09:52 -0500

    Setup-Simulator

--- DmvSimulator.java
+++ DmvSimulator.java
@@ -1,5 +1,30 @@
+import java.util.Random;
+import java.util.Scanner;
 public class DmvSimulator {
     public static void main(String[] args){
+        Scanner kb = new Scanner(System.in);
+        System.out.println("Welcome to the DMV");
+        //Random number
+        Random r = new Random();
+        
+        int randomNum = r.nextInt((100 - 1) + 1) + 1;
+        System.out.println("Your number is: " + randomNum);
+        CountDown(randomNum);
+        CountUp(randomNum);
+        System.out.println("You dont have the required paper work!");
+    }
+    public static void CountDown(int randomNum){
+        for(int i = randomNum + 1; i <= 100; i++){
+           
+            System.out.println(i);
+        }
+        
+    }
+    public static void CountUp(int randomNum){
+        for(int i = 1; i <= randomNum; i++){
+           
+            System.out.println(i);
+        }
         
     }
 }
